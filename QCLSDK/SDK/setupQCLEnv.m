5eb7ca62b542bffa6095fa9c9f587052924dcb16
sgr <sgrlabinfrared@gmail.com>
2018-12-14 17:16:59 -0500

    Testing new editor.

--- QCLSDK/SDK/setupQCLEnv.m
+++ QCLSDK/SDK/setupQCLEnv.m
@@ -5,7 +5,7 @@ dir = fileparts(mfilename('fullpath'));
 
 hfile = strcat(dir,'\MIRcatSDK.h'); % 'C:\Users\INFRARED\Documents\MATLAB\QCLSDK\SDK\MIRcatSDK.h'; % Denote the location of the headerfile 
 
-fprintf('========================================================\n');
+fprintf('=======================================================\n');
 fprintf('Loading MIRcatSDK Library\n');
 
 %[notfound, warnings] = loadlibrary('C:\Users\INFRARED\Documents\MATLAB\QCLSDK\SDK\Win32\MIRcatSDK', hfile, 'alias', 'MIRcatSDK');
